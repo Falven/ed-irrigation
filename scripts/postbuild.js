badefa37ff15d2562863ef863c0bafc3bfac6406
Fran Aguilera <fraguile@microsoft.com>
2021-05-19 09:35:20 -0400

    Updated script to take in build path arg.

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -1,22 +1,32 @@
+import * as path from "path";
 import * as fs from "fs";
 import * as os from "os";
 
-let arduino = JSON.parse(fs.readFileSync(".vscode/arduino.json"));
-let compileCommands = JSON.parse(
-  fs.readFileSync(`${arduino.output}/compile_commands.json`)
+const buildPath = process.argv[2];
+if (!buildPath) {
+  process.exit(-1);
+}
+const compilationDatabasePath = path.resolve(
+  buildPath,
+  "compile_commands.json"
 );
-if (compileCommands && compileCommands.length > 0) {
-  if (os.platform() == "win32") {
-    fixCompilerArgs(compileCommands);
-  }
-
-  fs.writeFileSync(
-    `${arduino.output}/compile_commands.json`,
-    JSON.stringify(compileCommands, null, 2)
-  );
+let compileCommands = JSON.parse(fs.readFileSync(compilationDatabasePath));
+if (!compileCommands) {
+  process.exit(-1);
 }
 
-function fixCompilerArgs(compileCommands) {
+if (compileCommands.length > 0) {
+  if (os.platform() == "win32") {
+    fixWindowsCompilerArgs(compileCommands);
+
+    fs.writeFileSync(
+      compilationDatabasePath,
+      JSON.stringify(compileCommands, null, 2)
+    );
+  }
+}
+
+function fixWindowsCompilerArgs(compileCommands) {
   compileCommands.forEach((command) => {
     if (command.arguments.length > 0) {
       if (!command.arguments[0].endsWith(".exe")) {
